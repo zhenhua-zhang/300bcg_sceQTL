7687147cd70e04da66a616450242437e7fd9f238
zhenhua-zhang <zhenhua.zhang217@gmail.com>
2023-01-23 10:36:20 +0100

    Update QTL mapping pipeline

--- bash/run_gemma.sh
+++ bash/run_gemma.sh
@@ -1,47 +1,70 @@
 #!/bin/bash
+#SBATCH --mem 4G
+#SBATCH --array 1-5
+#SBATCH --time 5:0:0
+#SBATCH --partition cpu
+#SBATCH --cpus-per-task 4
+#SBATCH --output %A_%a-run_gemma.log
 
 proj_dir=~/Documents/projects/wp_bcg_eqtl
-pct=Monocytes
 
 source $proj_dir/scripts/.env/bin/activate
 
+celltype=Monocytes
+ia_term=time
+
 # Prepare inputs gene expression matrix
 python3 $proj_dir/scripts/py3/prepare_gemma_input.py \
-  -g abc \
-  -p abc \
-  -f /vol/projects/zzhang/projects/wp_bcg_eqtl/inputs/reference/Gencode/gencode.v41.basic.annotation.gff3.gz \
-  -c abc \
-  -o $proj_dir/temps/mean_genotype.geno
+  -g $proj_dir/inputs/genotypes/300BCG_sub40_imp_hg38_ids_clean.vcf.gz \
+  -p $proj_dir/inputs/pseudo_bulk/$celltype/phenotypes.tsv \
+  -c $proj_dir/inputs/pseudo_bulk/$celltype/covariates_wpeer.tsv \
+  -m $proj_dir/inputs/pseudo_bulk/$celltype/sample_mapping.tsv \
+  -f $proj_dir/inputs/reference/Gencode/gencode.v41.basic.annotation.Ec.transcript.autosome.gff.gz \
+  -I $ia_term \
+  -o $proj_dir/temps/$celltype
 
-#
-vcfpath=$proj_dir/inputs/genotypes/300BCG_sub40_imp_hg38_ids_clean.vcf.gz
-plink --vcf $vcfpath --recode bimbam --out $proj_dir/outputs/genotypes/BIMBAM/300BCG_sub40_imp_hg38_clean
+# Combined all genotypes and annotations into one
+cat $proj_dir/temps/$celltype/block_*/*_genotype.txt >| $proj_dir/temps/genotype.txt
+cat $proj_dir/temps/$celltype/block_*/*_genotype_annotation.txt >| $proj_dir/temps/genotype_annotation.txt
 
-## Prepare genotypes
+# Relatedness matrix from all used genotypes
+gemma -gk 2 \
+  -g $proj_dir/temps/Gemma/genotype.txt \
+  -a $proj_dir/temps/Gemma/genotype_annotation.txt \
+  -p $proj_dir/temps/Gemma/blocks/block_00000/PNRC2_phenotype.txt \
+  -o kinship \
+  -outdir $proj_dir/temps/Gemma
 
 
-# Run GEMMA of linear mixed model
-gemma \
-  -g $snppath \
-  -p $exppath \
-  -a $annpath \
-  -k $kinpath \
-  -gxe $gbepath \
-  -lmm \
-  -notsnp \
-  -o $outdir
+for per_input in $proj_dir/temps/Gemma/blocks/block_*/*_genotype.txt; do
+  per_block=$(dirname $per_input)
+  per_gene=$(basename $per_input | cut -f1 -d_)
 
-gemma -gk \
-  -g mouse_hs1940.geno.txt.gz \
-  -p mouse_hs1940.pheno.txt \
-  -a mouse_hs1940.anno.txt \
-  -o mouse_hs1940
+  # Run GEMMA of linear mixed model
+  gemma -lmm 3 \
+    -g $per_block/${per_gene}_genotype.txt \
+    -a $per_block/${per_gene}_genotype_annotation.txt \
+    -p $per_block/${per_gene}_phenotype.txt \
+    -k $proj_dir/temps/Gemma/kinship.abs.cXX.txt \
+    -c $proj_dir/temps/Gemma/covariate.txt \
+    -o ${per_gene}_QTL_common \
+    -outdir $per_block
 
-gemma -lmm \
-  -g mouse_hs1940.geno.txt.gz \
-  -p mouse_hs1940.pheno.txt \
-  -a mouse_hs1940.anno.txt \
-  -k output/mouse_hs1940.cXX.txt \
-  -n 1 \
-  -gxe mouse_hs1940.gxe.txt \
-  -o mouse_hs1940_CD8MCH_lmm
+  gemma -lmm 1 \
+    -g $per_block/${per_gene}_genotype.txt \
+    -a $per_block/${per_gene}_genotype_annotation.txt \
+    -p $per_block/${per_gene}_phenotype.txt \
+    -k $proj_dir/temps/Gemma/kinship.abs.cXX.txt \
+    -c $proj_dir/temps/Gemma/covariate.txt \
+    -o ${per_gene}_QTL_interaction \
+    -gxe $proj_dir/temps/Gemma/interaction_covariates-$ia_term.txt \
+    -outdir $per_block
+
+  break
+
+  if [[ $count -le 8 ]]; then
+    count=$(( $count + 1 )) && continue
+  else
+    count=0 && wait
+  fi
+done
--- bash/run_limix_qtl.sh
+++ bash/run_limix_qtl.sh
@@ -20,16 +20,45 @@ check_term() {
 
 # Debug mode.
 DEBUG=false
-
-if [[ $DEBUG == true ]]; then
-  njobs=2
-else
-  njobs=200
-fi
+if [[ $DEBUG == true ]]; then njobs=2; else njobs=500; fi
 
 
 # Working path.
 proj_dir=~/Documents/projects/wp_bcg_eqtl
+
+
+#
+## Create chunks file
+#
+if [[ ! -e $proj_dir/inputs/annotations/chunks_file.txt ]]; then
+  annfile=$proj_dir/inputs/annotations/annotations_hg38.tsv
+  chunkfile=$proj_dir/inputs/annotations/chunks_file.txt
+
+  Rscript - $annfile $chunkfile <<'EOF'
+library(magrittr)
+
+options <- commandArgs(trailingOnly = TRUE)
+if (length(options) != 2) stop("Require two positional options: in-file and out-file!")
+
+anntab <- data.table::fread(options[1])
+max_pos <- max(anntab$start, anntab$end) + 1000
+anntab %>%
+  dplyr::mutate(tss = dplyr::if_else(feature_strand == "+", start, end)) %>%
+  dplyr::arrange(chromosome, tss) %>%
+  dplyr::group_by(chromosome) %>%
+  dplyr::mutate(group_id = dplyr::row_number() %/% 15) %>%
+  dplyr::group_by(chromosome, group_id) %>%
+  dplyr::summarize(start = min(tss) - 1000) %>%
+  dplyr::ungroup() %>%
+  dplyr::group_by(chromosome) %>%
+  dplyr::mutate(start = start, end = dplyr::lead(start, default = max_pos) - 1, chunk = paste0(chromosome, ":", start, "-", end)) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(chunk) %>%
+  data.table::fwrite(options[2], col.names = FALSE)
+
+cat("Done\n")
+EOF
+fi
 
 
 # Prepare inputs for each pairs
@@ -96,9 +125,12 @@ smfile=$proj_dir/scripts/snakemake/300bcg_limix_qtl.smk
 #
 # Submit jobs to do the regression analysis.
 mode=pseudo_bulk
-for model in normal; do
+for model in normal interaction; do
+  model=interaction
   for ctype in Monocytes CD4T CD8T NK B; do
+    ctype=CD4T
     out_dir=$proj_dir/outputs/$mode/$model/$ctype
+    echo $out_dir $tmstmp
 
     # Save the log files.
     if [[ ! -d $out_dir/logs ]]; then mkdir -p $out_dir/logs; fi
@@ -116,10 +148,12 @@ for model in normal; do
       outf=$out_dir/logs/%j-${tmstmp}_${model}_sbatch.out
       snakemake -k -j $njobs -w 60 -s $smfile \
         -C runMode=$mode cellType=$ctype compPair=. evalModel=$model usePEER=true \
-        --cluster 'sbatch -t 1:59:0 -p cpu -o '$outf' -e '$errf' -J '$model-$ctype' -N 1 --cpus-per-task 1 --ntasks 1 --mem 4G'
+        --cluster 'sbatch -t 4:59:0 -p cpu -o '$outf' -e '$errf' -J '$model-$ctype' -N 1 --cpus-per-task 1 --ntasks 1 --mem 8G'
     else
       # Runs pair-wise comparison for the "interaction" model.
-      for cmppair in {T0_LPS,T3m_LPS,T3m_RPMI}.vs.T0_RPMI T3m_LPS.vs.T3m_RPMI; do
+      # for cmppair in {T0_LPS,T3m_LPS,T3m_RPMI}.vs.T0_RPMI T3m_LPS.vs.T3m_RPMI; do
+      for cmppair in {T0_LPS,T3m_RPMI}.vs.T0_RPMI T3m_LPS.vs.T3m_RPMI; do
+        cmppair=T3m_RPMI.vs.T0_RPMI
         icterm=$(check_term $cmppair)
 
         # Snakemake dry-run log.
@@ -129,11 +163,11 @@ for model in normal; do
           1>&2 >| $dr_log
 
         # Run
-        errf=$out_dir/logs/%j-${tmstmp}_${model}_${cmppair}_sbatch.err
-        outf=$out_dir/logs/%j-${tmstmp}_${model}_${cmppair}_sbatch.out
+        errf=$out_dir/logs/%j-%x-${tmstmp}_${model}_${cmppair}_sbatch.err
+        outf=$out_dir/logs/%j-%x-${tmstmp}_${model}_${cmppair}_sbatch.out
         snakemake -k -j $njobs -w 60 -s $smfile \
           -C runMode=$mode cellType=$ctype compPair=$cmppair evalModel=$model interTerm=$icterm usePEER=true \
-          --cluster 'sbatch -t 1:59:0 -p cpu -o '$outf' -e '$errf' -J '$model-$ctype-$cmppair' -N 1 --cpus-per-task 1 --ntasks 1 --mem 4G'
+          --cluster 'sbatch -t 11:59:0 -o '$outf' -e '$errf' -J '$model-$ctype-$cmppair' -N 1 --cpus-per-task 1 --ntasks 1 --mem 8G'
       done
     fi
   done
@@ -168,3 +202,31 @@ for per_win in win_4; do
       --cluster 'sbatch -t 1:59:0 -p cpu -o '$outf' -e '$errf' -J '$mode-$ctype' -N 1 --cpus-per-task 1 --ntasks 1 --mem 4G'
   done
 done
+
+
+# TODO: remove me
+# singularity exec /home/zzhang/Documents/projects/wp_bcg_eqtl/scripts/tools/limix20220510.simg \
+#   Rscript \
+#   /home/zzhang/Documents/projects/wp_bcg_eqtl/scripts/tools/limix_qtl/Limix_QTL/post_processing/multTestCorrect_PipeLine.R \
+#   /home/zzhang/Documents/projects/wp_bcg_eqtl/outputs/pseudo_bulk/interaction/Monocytes/T3m_RPMI.vs.T0_RPMI/ \
+#   /home/zzhang/Documents/projects/wp_bcg_eqtl/outputs/pseudo_bulk/interaction/Monocytes/T3m_RPMI.vs.T0_RPMI/ \
+#   0.05
+# 
+# singularity exec /home/zzhang/Documents/projects/wp_bcg_eqtl/scripts/tools/limix20220510.simg \
+#   python /home/zzhang/Documents/projects/wp_bcg_eqtl/scripts/tools/limix_qtl/Limix_QTL/post_processing/minimal_postprocess.py \
+#   -tfb -sfo \
+#   -id /home/zzhang/Documents/projects/wp_bcg_eqtl/outputs/pseudo_bulk/interaction/Monocytes/T3m_RPMI.vs.T0_RPMI \
+#   -od /home/zzhang/Documents/projects/wp_bcg_eqtl/outputs/pseudo_bulk/interaction/Monocytes/T3m_RPMI.vs.T0_RPMI
+# 
+# singularity exec /home/zzhang/Documents/projects/wp_bcg_eqtl/scripts/tools/limix20220510.simg \
+#   python /home/zzhang/Documents/projects/wp_bcg_eqtl/scripts/tools/limix_qtl/Limix_QTL/post_processing/minimal_postprocess.py \
+#   -tfb -sfo \
+#   -id /home/zzhang/Documents/projects/wp_bcg_eqtl/outputs/pseudo_bulk/interaction/B/T0_LPS.vs.T0_RPMI \
+#   -od /home/zzhang/Documents/projects/wp_bcg_eqtl/outputs/pseudo_bulk/interaction/B/T0_LPS.vs.T0_RPMI
+# 
+# singularity exec /home/zzhang/Documents/projects/wp_bcg_eqtl/scripts/tools/limix20220510.simg \
+#   Rscript \
+#   /home/zzhang/Documents/projects/wp_bcg_eqtl/scripts/tools/limix_qtl/Limix_QTL/post_processing/multTestCorrect_PipeLine.R \
+#   /home/zzhang/Documents/projects/wp_bcg_eqtl/outputs/pseudo_bulk/interaction/B/T0_LPS.vs.T0_RPMI/ \
+#   /home/zzhang/Documents/projects/wp_bcg_eqtl/outputs/pseudo_bulk/interaction/B/T0_LPS.vs.T0_RPMI/ \
+#   0.05
