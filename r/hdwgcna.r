0eb406527c46d333c4704c410f8ed46519ed2e70
zhenhua-zhang <zhenhua.zhang217@gmail.com>
2023-09-06 10:53:35 +0200

    remove temporary script

--- r/hdwgcna.r
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env Rscript
-options(stringsAsFactors = FALSE, data.table.verbose = FALSE, error = NULL)
-suppressPackageStartupMessages({
-  library(hdwgcna)
-  library(tidyverse)
-  library(data.table)
-})
